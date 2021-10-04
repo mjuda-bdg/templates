e3ae517c2bdb5d3896d6a2f96115315425aa33fc
mjuda-sii <90615322+mjuda-sii@users.noreply.github.com>
2021-10-04 15:00:12 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,2 @@
-Select-AzSubscription -SubscriptionId d130c0b6-8dec-43f8-8b17-8fa8310a8e5e
+Select-AzSubscription -SubscriptionId 'd130c0b6-8dec-43f8-8b17-8fa8310a8e5e'
 Stop-AzVM -ResourceGroupName 'rg-vm-dev-01' -Name 'vmtestsrv01' -Force
