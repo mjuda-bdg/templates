f248b52f07477cb011a9d5e2f15786eb3a9899b0
mjuda-sii <90615322+mjuda-sii@users.noreply.github.com>
2021-10-04 15:38:19 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,5 @@
 Clear-AzContext
+Connect-AzAccount -TenantId d30ae693-95a7-427c-ab10-df22595e61ff -SubscriptionId d130c0b6-8dec-43f8-8b17-8fa8310a8e5e
 Set-AzContext -TenantId d30ae693-95a7-427c-ab10-df22595e61ff -SubscriptionId d130c0b6-8dec-43f8-8b17-8fa8310a8e5e
 Select-AzSubscription -SubscriptionId d130c0b6-8dec-43f8-8b17-8fa8310a8e5e
 Stop-AzVM -ResourceGroupName 'rg-vm-dev-01' -Name 'vmtestsrv01' -Force
