c3400beabad6323637ba5e6709d934b722481b21
mjuda-sii <90615322+mjuda-sii@users.noreply.github.com>
2021-10-05 12:49:58 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,1 @@
 Start-AzVM -ResourceGroupName 'rg-vm-dev-01' -Name 'vmtestsrv01'
-Start-AzAksCluster --name 'aks-test-dev-01' --resource-group 'rg-aks-dev-01'
-Start-AzWebApp -ResourceGroupName 'rg-application-dev-01' -Name 'app-dev-01'
-Start-AzFunctionApp -Name 'funcapp-dev-01' -ResourceGroupName 'rg-application-dev-01'
