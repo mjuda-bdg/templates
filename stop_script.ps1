0b89875334075dbb17a5ca77f7bb0ddab96ecaa6
mjuda-sii <90615322+mjuda-sii@users.noreply.github.com>
2021-10-05 14:24:03 +0200

    Update stop_script.ps1

--- stop_script.ps1
+++ stop_script.ps1
@@ -1,5 +1,4 @@
-Stop-AzVM -ResourceGroupName 'rg-vm-dev-01' -Name 'vmtestsrv01'
-Start-Sleep -Seconds 120
-Stop-AzAksCluster -Name 'aks-test-dev-01' -ResourceGroupName 'rg-aks-dev-01'
+Stop-AzFunctionApp -Name 'funcapp-dev-01' -ResourceGroupName 'rg-application-dev-01' -Force
 Stop-AzWebApp -ResourceGroupName 'rg-application-dev-01' -Name 'app-dev-01'
-Stop-AzFunctionApp -Name 'funcapp-dev-01' -ResourceGroupName 'rg-application-dev-01'
+Stop-AzAksCluster -Name 'aks-test-dev-01' -ResourceGroupName 'rg-aks-dev-01'
+Stop-AzVM -ResourceGroupName 'rg-vm-dev-01' -Name 'vmtestsrv01' -Force
